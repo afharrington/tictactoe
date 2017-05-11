58ed7d5bbd6a586c5895b10c78719436de002002
Anna Harrington <annafkh@gmail.com>
2017-05-10 22:14:48 -0400

    Added simple AI 'Tom'

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
 // Game State
 var gameActive = false;
 var selectedThisTurn, board;
-var playerMode = "one"; 
-var currentPlayer = "X"; 
+var playingTom = false;
+var currentPlayer = "X";
+
+var xName, oName;
+if (playingTom) {
+  xName = "You";
+  oName = "Tom";
+} else {
+  xName = "Player X";
+  oName = "Player O";
+}
 
 // UI Elements
 var startUI = document.getElementById('start');
+var tomUI = document.getElementById('tom');
+
 var submitUI = document.getElementById('submit');
 var playAgainUI = document.getElementById('play-again');
 var boardUI = document.getElementById('board');
 var messageUI = document.getElementById('message');
-var squaresUI = document.querySelectorAll(".square");
+var squaresUI = document.querySelectorAll('.square');
 var header = document.getElementById('header');
+
+var startScreenButtons = document.querySelectorAll('.start-screen');
 
 // Event Handlers
 startUI.onclick = startGame;
+tomUI.onclick = function(){
+  playingTom = true;
+  startGame();
+}
+
 submitUI.onclick = submitMove;
 for (var i=0; i<squaresUI.length; i++) {
   squaresUI[i].onclick = function() {
-    selectSquare(this.id);
+    if (currentPlayer !== "Tom") {
+      selectSquare(this.id);
+    }
   }
 }
-playAgainUI.onclick = resetGameUI; 
+playAgainUI.onclick = resetGameUI;
 
 function startGame() {
   // Updates UI elements for play mode
   boardUI.style.display = "block";
   submitUI.style.display = "inline-block";
-  startUI.style.display = "none";
-  header.style.display = "none";
-  messageUI.innerHTML = "Player "+ currentPlayer + "'s Turn";
+
+  for (var i=0; i<startScreenButtons.length; i++) {
+    startScreenButtons[i].style.display = "none";
+  }
+
+  if (currentPlayer == "X" && playingTom) {
+    messageUI.innerHTML = "Your Turn";
+  } else if (currentPlayer == "X" && !playingTom) {
+    messageUI.innerHTML = "Player X's Turn"
+  } else if (currentPlayer == "O" && playingTom) {
+    messageUI.innerHTML = "Tom's Turn";
+  } else {
+    messageUI.innerHTML = "Player O's Turn"
+  }
 
   for (var i=0; i<squaresUI.length; i++) {
     squaresUI[i].innerHTML = "";
@@ -37,14 +68,19 @@ function startGame() {
 
   board = createEmptyBoard();
   gameActive = true;
-}
 
+  if (playingTom && currentPlayer == "O") {
+    tomsMove();
+  }
+}
 
 // Fills square when selected and ensures one selection per turn
 function selectSquare(squareId) {
-  var square = document.getElementById(squareId); 
+  var square = document.getElementById(squareId);
+  var playerMark;
+  currentPlayer == "X" ? playerMark = "X" : playerMark = "O";
 
-  if (selectedThisTurn) {  
+  if (selectedThisTurn) {
     // Grabs HTML element for previously selected square
     var selectedSquare = document.getElementById(selectedThisTurn);
 
@@ -54,7 +90,7 @@ function selectSquare(squareId) {
     }
   }
 
-  // Only if the square is empty, fill with the player's symbol 
+  // Only if the square is empty, fill with the player's symbol
   if (square.innerHTML == "") {
     square.innerHTML = currentPlayer;
     selectedThisTurn = squareId;
@@ -64,7 +100,6 @@ function selectSquare(squareId) {
 
 // Adds selected square to the board data, ends player's turn
 function submitMove() {
-  var winner, full;
 
   if (!selectedThisTurn) {
     messageUI.innerHTML ="Select a square and try again";
@@ -77,32 +112,22 @@ function submitMove() {
         case "b":
           return 1;
         case "c":
-          return 2;        
+          return 2;
       }
-    })(); 
-   // Adds move to board data
-    board[selectedRow][selectedCol] = currentPlayer;
-
-    winner = checkForWin();
-
-    if (winner == "X" || winner == "O") {
-      endGame("Player " + winner + " won!");
-    } else {
-      full = checkForFull();
-    }
-
-    if (full) {
-      endGame("No winner this time.");
-    };
-
-    if (gameActive) {
-      switchPlayer();
-      selectedThisTurn = null;
-    }
-  }  
+    })();
+  }
+  // Adds move to board data
+  board[selectedRow][selectedCol] = currentPlayer;
+  checkForWin();
+  if (gameActive) {
+    switchPlayer();
+    selectedThisTurn = null;
+  }
 }
 
+
 function checkForWin() {
+  var winner;
   var winningRuns = {
     row0: [ [board[0][0]], [board[0][1]], [board[0][2]] ],
     row1: [ [board[1][0]], [board[1][1]], [board[1][2]] ],
@@ -118,25 +143,34 @@ function checkForWin() {
   for (run in winningRuns) {
     var xCount = 0;
     var oCount = 0;
-
     // for each item in the run, check if value is X or O
     for (var i=0; i<3; i++) {
       if (winningRuns[run][i] == "X") {
         xCount++;
       } else if (winningRuns[run][i] == "O") {
         oCount++;
-      } 
+      }
     }
     if (xCount == 3) {
-      return "X";
+      winner = "X";
     } else if (oCount == 3) {
-      return "O";
+      winner = "O";
     }
+  }
+
+  if (winner == "X" || winner == "O") {
+    endGame(winner);
+  } else {
+    full = checkForFull();
+  }
+
+  if (full) {
+    endGame("None");
   }
 }
 
 function checkForFull() {
-  nullCount = 0; 
+  nullCount = 0;
 
   for (row=0; row<3; row++) {
     for (col=0; col<3;col++) {
@@ -152,23 +186,90 @@ function checkForFull() {
 
 function switchPlayer() {
   currentPlayer == "X" ? currentPlayer = "O" : currentPlayer = "X";
-  messageUI.innerHTML = "Player "+ currentPlayer + "'s Turn";
+  if (playingTom) {
+    if (currentPlayer == "O"){
+      messageUI.innerHTML = "Tom's Turn";
+      submitUI.style.display = "none";
+      tomsMove();
+    } else {
+      submitUI.style.display = "inline-block";
+      messageUI.innerHTML = "Your Turn";
+    }
+  } else if (!playingTom) {
+    messageUI.innerHTML = "Player "+ currentPlayer + "'s Turn";
+  }
 }
 
-function endGame(message) {
+function tomsMove() {
+  var potentialMoves = [];
+  for (row=0; row<3; row++) {
+    for (col=0; col<3;col++) {
+      if (board[row][col] == null) {
+        potentialMoves.push([row, col]);
+      }
+    }
+  }
+
+  randomSelection = Math.floor((Math.random() * potentialMoves.length));
+  var move = potentialMoves[randomSelection];
+
+  var row = move[0];
+  var col = move[1];
+  board[row][col] = "O";
+
+  setTimeout(function(){
+    drawTomsMove(row, col);
+    checkForWin();
+    if (gameActive) {
+      switchPlayer();
+      selectedThisTurn = null;
+    }
+  }, 1000);
+}
+
+// draws Tom's move to the board
+function drawTomsMove(row, col) {
+  var row = (function() {
+    switch(row) {
+      case 0:
+        return "a";
+      case 1:
+        return "b";
+      case 2:
+        return "c";
+    }
+  })();
+
+  var moveId = row + col;
+  var square = document.getElementById(moveId);
+  square.innerHTML = "O";
+}
+
+function endGame(winner) {
+  console.log("Passed to endGame(): ", winner);
   gameActive = false;
-  messageUI.innerHTML = message;
+
+  if (winner == "X" && playingTom) {
+    messageUI.innerHTML = "You won!";
+  } else if (winner == "X" && !playingTom) {
+    messageUI.innerHTML = "Player X won!"
+  } else if (winner == "O" && playingTom) {
+    messageUI.innerHTML = "Tom won";
+  } else {
+    messageUI.innerHTML = "Player O won!"
+  }
+
   submitUI.style.display = "none";
   playAgainUI.style.display = "inline-block";
 }
 
-// Brings back to the start screen 
+// Brings back to the start screen
 function resetGameUI() {
   boardUI.style.display = "none";
   playAgainUI.style.display = "none";
   startUI.style.display = "inline-block";
   header.style.display = "inline-block";
-  messageUI.innerHTML = "Get ready, Player " + currentPlayer + "...";
+  messageUI.innerHTML = "Winner goes first...";
 }
 
 function createEmptyBoard() {
